2a906d4d0b1494cd7c6e93cff04b8352352b0c19
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2023-11-30 19:40:04 +0200

    master

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
-word_last_2 = word[:-1]
-middle_word = word[8:9]
+word_last_2 = word[7:]
+middle_word = word[1:8]
 print(f"First letters:  {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word:  {middle_word}")
