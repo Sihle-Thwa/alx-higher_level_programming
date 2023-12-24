1b9c2fabbd62ad6a4deb5bf270a8553c801eb1cb
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2023-12-24 17:36:32 +0200

    Create 100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+Can't create, edit, or upload … You don't have enough storage to create, edit, and upload files. Get 100 GB of storage for R 34.99 R 8.99/month for 3 months.
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
