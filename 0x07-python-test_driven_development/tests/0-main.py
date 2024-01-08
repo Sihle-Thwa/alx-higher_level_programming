10c8e4a4f46af8777f7c3467d61ce4cad1d5bb1c
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-01-08 18:58:05 +0200

    Create 0-main.py

---
+++ 0x07-python-test_driven_development/tests/0-main.py
@@ -0,0 +1,16 @@
+Can't create, edit, or upload … You don't have enough storage to create, edit, and upload files. Get 100 GB of storage for R 34.99 R 8.99/month for 3 months.
+#!/usr/bin/python3
+add_integer = __import__('0-add_integer').add_integer
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
